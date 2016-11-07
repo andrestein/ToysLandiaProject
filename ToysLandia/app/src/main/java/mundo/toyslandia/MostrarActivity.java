582863d5a9913a57e9b6c1242d185c17b3a136b3
Shad4070 <oscar4070@hotmail.com>
2016-11-07 03:47:04 -0500

    los malditos puntos suspensivos(mostrar)

---
+++ ToysLandia/app/src/main/java/mundo/toyslandia/MostrarActivity.java
@@ -0,0 +1,54 @@
+package mundo.toyslandia;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import DataBase.DataBaseMananger;
+import DataBase.DbHelper;
+
+public class MostrarActivity extends AppCompatActivity {
+    private static DataBaseMananger manan;
+    private static Cursor cursor;
+    private ListAdapter adap;
+    ListView lvItems;
+    ArrayList<String> datos=new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar);
+        lvItems=(ListView) findViewById(R.id.lvItems);
+
+        //Comprobando si hay items
+
+        datos=manan.cargarCursorProductos();
+       // datos.add("1");
+
+        if(!datos.isEmpty()) {
+            adap = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datos);
+            lvItems.setAdapter(adap);
+        }else{
+            Toast.makeText(getApplicationContext(),"No hay ningun item",Toast.LENGTH_LONG).show();
+        }
+        /*
+        String[]from=new String []{"SElECT SELECT NOM_SUC FROM SUCITEM","SELECT COD_ITE FROM SUCITEM","SELECT STOCK FROM SUCITEM"};
+        int[]to=new int[]{android.R.id.text1,android.R.id.text2};
+        manan=new DataBaseMananger(this);
+        cursor=manan.cargarC();
+        adap=new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,cursor,from,to,0);
+
+        lvItems.setAdapter(adap);*/
+
+
+    }
+}
